2bd679cf2f8396cc043d625e61953ed3d385a73c
randomizerkatt <ABroderick@student.neumont.edu>
2020-02-13 15:46:38 -0700

    Added location. Started with connecting to API.

---
+++ app/src/main/java/team7/finderfeast/RestaurantConnection.java
@@ -0,0 +1,8 @@
+package team7.finderfeast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestaurantConnection extends AppCompatActivity {
+    private String url = "https://developers.zomato.com/api/v2.1/";
+    private String keyCode = "d3b965d17f5d9cdd0c08e4d1d6ed47e2";
+}
