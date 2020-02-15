0c993c37ca297c6dee2d70b96cdfeab436dfde13
tikalaka <baydadan001@gmail.com>
2020-02-14 19:36:41 -0700

    Mongo Connection (NOT FINISHED)

--- app/src/main/java/team7/finderfeast/RestaurantConnection.java
+++ app/src/main/java/team7/finderfeast/RestaurantConnection.java
@@ -1,8 +1,29 @@
 package team7.finderfeast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantConnection extends AppCompatActivity {
     private String url = "https://developers.zomato.com/api/v2.1/";
     private String keyCode = "d3b965d17f5d9cdd0c08e4d1d6ed47e2";
+    MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+    MongoClient mongoClient = new MongoClient(connectionString);
+
 }
